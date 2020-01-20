4aa3607c015af9430240ca15ac8d0affef75dbef
sientifiko <socialscienceanime@gmail.com>
2020-01-20 18:01:48 -0300

    modelando CI, p.64

--- intro_inferencia.R
+++ intro_inferencia.R
@@ -143,7 +143,8 @@ ggplot(dat.f,aes(sample = Bodyweight)) +
   labs(x = "Cuantiles teóricos", y = "Cuantiles observados")
 
 
-# Simulamos distribución para múltiples muestreos
+# Simulamos distribución para múltiples muestreos bajo supuesto de 
+# población conocida, y por lo tanto su desviación
 Ns <- c(3, 12, 25, 50)
 res <- sapply(Ns, function(n){
   replicate(10000, {
@@ -176,17 +177,99 @@ ggplot(df.res, aes(sample =.)) +
   facet_wrap(.~N, nrow = 2) +
   labs(x = "Cuantiles teóricos", y = "Cuantiles observados")
 
+# Pero bajo supuesto de población desconocida,con desviación 
+# estándar estimada <- t de student
+
+t.student.diff.n <- function(n){
+  y <- sample(tratamiento.pop,n)
+  x <- sample(control.pop,n)
+  t.test(y, x)$statistic
+}
+
+# simulación de Monte Carlo de t de student, para distintos N
+res <- sapply(Ns, function(n){
+  replicate(10000, t.student.diff.n(n))
+})
+
+# reconvertir a data frame
+res <- res %>% as.data.frame()
+n.label <- c("N_3", "N_12", "N_25", "N_50")
+lista <- list()
+for (i in seq(along=n.label)) {
+  temp.res <- res[,i] %>% as.data.frame()
+  temp.res$N <- n.label[i]
+  lista[[i]] <- temp.res
+}
+
+# convertir lista a df y reorganizar factores
+df.res <- do.call("rbind", lista)
+df.res$N <- df.res$N %>% as.factor()
+df.res$N <- factor(df.res$N, levels(df.res$N)[c(3,1,2,4)])
+
+# eliminar objetos a no ultilizar
+rm(t.student.diff.n)
+rm(i, n.label, res, lista, temp.res)
+
+# qq plot estimada por t de student, para distintos N
+ggplot(df.res, aes(sample =.)) +
+  geom_qq() +
+  geom_qq_line() +
+  facet_wrap(.~N, nrow = 2) +
+  labs(x = "Cuantiles teóricos", y = "Cuantiles observados t-student")
 
 
+# == PRUEBA T DE STUDENT
+# (dataset específico de ratas hembras)
+dat <- read.csv("femaleMiceWeights.csv")
 
+# capturar hembras en dieta de control
+control.pop <- dat %>% filter(Diet == "chow") %>%
+  dplyr::select(Bodyweight) %>% unlist() 
 
+# capturar hembras bajo hf
+tratamiento.pop <- dat %>% filter(Diet == "hf") %>%
+  dplyr::select(Bodyweight) %>% unlist()
 
+# analizando función t de student, t.test()
+resultado <-t.test(dat$Bodyweight[dat$Diet=="hf"], 
+                   dat$Bodyweight[dat$Diet=="chow"])
 
+# imprimir resultado
+resultado
 
+# como se ve ésta diferencia bajo distribución normal??
+pnorm(resultado$statistic, lower.tail = F) * 2 # por 2 porque distribución es
+                                               # simétrica
 
+# diferencias?? Si, supuestos de CLT y t-student son distintos. 
+# CLT -> dice que hay efecto cuando no lo hay (error de tipo I)
+# t student -> dice que NO hay efecto cuando SI lo hay (error de tipo II)
 
+# == INTERVALOS DE CONFIANZA EN T DE STUDENT
 
+# imprimir CI
+resultado$conf.int
 
+# calculando CI a mano
+# recorda que operamos bajo intérvalo de 
+# \bar{X}-Q*\frac{s_x}{\sqrt{N}}\leq\mu_x\leq\bar{X}+Q*\frac{s_x}{\sqrt{N}}  
+
+# importamos población de ratas
+dat <- read.csv("mice_pheno.csv")
+
+# extramos población de ratas femeninas en dieta normal
+pop.control <- dat %>% 
+  filter(Sex == "F", Diet == "chow") %>% 
+  dplyr::select(Bodyweight)
+
+# muestra de ratas
+muestra <- sample(pop.control$Bodyweight, 30)
+
+# calculamos parámetros
+media <- mean(muestra) # \bar{X}
+Q <- qnorm(1 - 0.05/2) # Q
+se <- sd(muestra)/sqrt(30) # \frac{s_x}{\sqrt{N}}
+intervalo <- c(media - Q*se, media + Q*se) # \mu_x\pm\bar{X}+Q*\frac{s_x}{\sqrt{N}} 
 
 
 
